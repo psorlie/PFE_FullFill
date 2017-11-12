e2f1f5f933b5c5b17985321f7e924b5a19892707
Sorlie <sorlie.pierrefrancois@gmail.com>
2017-11-12 18:36:21 +0100

    ADD - Another try

---
+++ VracBerry/C/src/VracBerry_Server.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : VracBerry_Server.c
+ Author      : Fullfill_team
+ Version     :
+ Copyright   : BSD-2clauses
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
