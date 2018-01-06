8be10c170308c5f491ef64f9a27dbd3784e40ad0
Sorlie <sorlie.pierrefrancois@gmail.com>
2018-01-06 18:02:59 +0100

    ADD - More commenting on the dispenserManager header

--- VracBerry/C/src/DispenserManager.h
+++ VracBerry/C/src/DispenserManager.h
@@ -15,10 +15,26 @@
 #include "Network_Configuration.h"
 #include "Dispenser.h"
 
+/**
+ * @def type for the head of the list
+ */
 typedef struct Dispenser_list_t Dispenser_list;
 
+
+/**
+ * @brief function to get the first dispenser of the list
+ *
+ * @retval Dispenser* the first dispenser.
+ */
 extern Dispenser* DispenserManager_get_list();
 
+/**
+ * @brief function to initialize the list (malloc of the head, ...)
+ *
+ * the list hasnt been initialized
+ *
+ * @retval void
+ */
 extern void DispenserManager_init();
 
 extern Dispenser* DispenserManager_add_dispenser(Dispenser_Id, char*, Battery, Filling);
@@ -77,20 +93,65 @@ extern void DispenserManager_send_warning_lost_dispenser(Dispenser_Id);
 
 extern void DispenserManager_prepare_destroy_dispenser(Dispenser_Id);
 
+/**
+ * @brief function called from the translator that had a dispenser with the data he sent
+ *
+ * @param[in] the id of the new dispenser
+ *
+ * @param[in] the battery value it sent
+ *
+ * @param[in] the filling value it sent
+ *
+ * @retval Dispenser* of the new dispenser
+ */
 extern Dispenser* DispenserManager_add_new_detected_dispenser(Dispenser_Id, Battery, Filling);
 
+/**
+ * @brief function called when the dispenser is in run state and an user ask the product name
+ *
+ * @pre the dispenser is in run state ?
+ *
+ * @param[in] Dispenser* of the asked dispenser
+ */
 extern void DispenserManager_ask_product_name(Dispenser*);
 
+/**
+ * @brief function to tell to a dispenser that he is broken
+ *
+ * @param[in] id of the dispenser
+ */
 extern void DispenserManager_tell_dispenser_broken(Dispenser_Id);
 
+/**
+ * @brief function to tell to a dispenser he has to repeat his last message.
+ *
+ * @param[in] id of the dispenser
+ */
 extern void DispenserManager_tell_dispenser_repeat(Dispenser_Id);
 
+
+/**
+ * @brief function called to save the data of a dispenser in the backup file
+ *
+ * @param[in] Dispenser* of the to-be saved dispenser
+ */
 extern void DispenserManager_save_in_backup(Dispenser*);
 
+/**
+ * @brief function called every morning to know if there is dirty dispenser
+ *
+ * @param[in] the maximum days since last cleaning
+ */
 extern void DispenserManager_check_dispenser_is_dirty(int);
 
+/**
+ * @brief function called to generate for every dispenser that the day is over
+ */
 extern void DispenserManager_end_of_the_day();
 
+/**
+ * @brief function called to generate for every dispenser that the night is over
+ */
 extern void DispenserManager_morning();
 
 
