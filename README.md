679abcf172031575f75e3e3af642862c12a9d0d2
LAPTOP-4A7SK83G\Clotilde <clotilde.sattler@laposte.net>
2018-02-05 23:46:03 +0100

    Un peu de ménage dans le git

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # PFE_FullFill
 Projet de Fin d'étude
-
-Il se passe des chosess
-Hahahahaaaa Merlin!!m,,mm,m,,m
--- VracBerry/Sockets/Websockets/Client/generate_percent.h
+++
@@ -1,6 +0,0 @@
-#ifndef __generate_percent_h__
-#define __generate_percent_h__
-
-size_t get_value(char* string_to_send);
-
-#endif /* #ifndef __generate_percent_h__ */
